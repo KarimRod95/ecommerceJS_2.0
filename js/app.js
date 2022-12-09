4f8fafcf2380f3c5e1404c3c9eed6aab480562db
Karim Rodriguez <karimrodriguezg95@gmail.com>
2022-12-09 12:08:11 -0300

    Se actualiza codigo y se agregan .json

--- js/app.js
+++ js/app.js
@@ -1,27 +1,26 @@
 //! clase producto
 
 class Producto {
-    constructor(id, nombre, precio, img) {
+    constructor(id, nombre, precio, categoria, img) {
         this.id = id
         this.nombre = nombre
         this.precio = precio
+        this.categoria = categoria
         this.img = img
     }
 
 }
 
-const producto1 = new Producto(1, 'iPhone', 600, "https://www.cronista.com/files/image/477/477549/631b5047dc3f7.jpg")
-//productosArray.push(new Producto(1, 'iIphone', 600))
-//productosArray.push(producto1)
-const producto2 = new Producto(2, 'iPad', 200, "https://m.media-amazon.com/images/I/81+N4PFF7jS._AC_SL1500_.jpg")
-const producto3 = new Producto(3, 'Airpods', 100, "https://i.insider.com/61d5c65a5a119b00184b1e1a?width=1136&format=jpeg")
-const producto4 = new Producto(4, 'Macbook', 1200, "https://d500.epimg.net/cincodias/imagenes/2022/08/29/gadgets/1661787731_142433_1661787875_noticia_normal.jpg")
-const producto5 = new Producto(5, 'AppleWatch', 250, "https://cloudfront-us-east-1.images.arcpublishing.com/infobae/436KLOUJFBBBDEOKEIG3NMOPVI.jpg")
-const producto6 = new Producto(6, 'Samsung S22', 600, "https://i0.wp.com/tecnopymes.com.ar/wp-content/uploads/2022/02/Galaxy-S22-y-S22-Plus.jpg?fit=1280%2C720&ssl=1")
-const producto7 = new Producto(7, 'Nokia 1100', 500, "https://www.lavoz.com.ar/resizer/edG1UQiaxBi8838gPcdUMczDJo4=/980x640/smart/cloudfront-us-east-1.images.arcpublishing.com/grupoclarin/FNZQQP6ECJCXZER4CADHKSOQOI.jpg")
-const producto8 = new Producto(8, 'Smart TV', 800, "https://www.naldo.com.ar/medias/504801.4-515Wx515H?context=bWFzdGVyfHJvb3R8MjEyMDV8aW1hZ2UvanBlZ3xoNjIvaDE1Lzk2NzA3MDQ4NTcxMTguanBnfGJhZDNiOGQwNzVhZTE0NjE5YjQ3NThlZDMwZTIwYjUyZmRlMGRmMDMyNjM4Yzk3ZDVhZTFlYzQ1YWVjMjc1NTY")
-const producto9 = new Producto(9, 'GoPro 10', 200, "https://www.cordobadigital.net/wp-content/uploads/2021/10/Gopro-hero-10-black.jpg")
-const producto10 = new Producto(10, 'Digital Camera', 550, "https://i5.walmartimages.com/asr/d2a51fe5-9998-4ca1-b811-e8d100b72829.8d07a2b24a7d9e061662780dc9fdfb12.jpeg")
+const producto1 = new Producto(1, 'iPhone', 600, "Celulares", "https://www.cronista.com/files/image/477/477549/631b5047dc3f7.jpg")
+const producto2 = new Producto(2, 'iPad', 200, "Accesorios", "https://m.media-amazon.com/images/I/81+N4PFF7jS._AC_SL1500_.jpg")
+const producto3 = new Producto(3, 'Airpods', 100, "Accesorios", "https://i.insider.com/61d5c65a5a119b00184b1e1a?width=1136&format=jpeg")
+const producto4 = new Producto(4, 'Macbook', 1200, "Computadoras", "https://d500.epimg.net/cincodias/imagenes/2022/08/29/gadgets/1661787731_142433_1661787875_noticia_normal.jpg")
+const producto5 = new Producto(5, 'AppleWatch', 250, "Accesorios", "https://cloudfront-us-east-1.images.arcpublishing.com/infobae/436KLOUJFBBBDEOKEIG3NMOPVI.jpg")
+const producto6 = new Producto(6, 'Samsung S22', 600, "Celulares", "https://i0.wp.com/tecnopymes.com.ar/wp-content/uploads/2022/02/Galaxy-S22-y-S22-Plus.jpg?fit=1280%2C720&ssl=1")
+const producto7 = new Producto(7, 'Nokia 1100', 500, "Celulares", "https://www.lavoz.com.ar/resizer/edG1UQiaxBi8838gPcdUMczDJo4=/980x640/smart/cloudfront-us-east-1.images.arcpublishing.com/grupoclarin/FNZQQP6ECJCXZER4CADHKSOQOI.jpg")
+const producto8 = new Producto(8, 'Smart TV', 800, "Televisor", "https://www.naldo.com.ar/medias/504801.4-515Wx515H?context=bWFzdGVyfHJvb3R8MjEyMDV8aW1hZ2UvanBlZ3xoNjIvaDE1Lzk2NzA3MDQ4NTcxMTguanBnfGJhZDNiOGQwNzVhZTE0NjE5YjQ3NThlZDMwZTIwYjUyZmRlMGRmMDMyNjM4Yzk3ZDVhZTFlYzQ1YWVjMjc1NTY")
+const producto9 = new Producto(9, 'GoPro 10', 200, "Camaras", "https://www.cordobadigital.net/wp-content/uploads/2021/10/Gopro-hero-10-black.jpg")
+const producto10 = new Producto(10, 'Digital Camera', 550, "Camaras", "https://i5.walmartimages.com/asr/d2a51fe5-9998-4ca1-b811-e8d100b72829.8d07a2b24a7d9e061662780dc9fdfb12.jpeg")
 
 
 //creando array productos
@@ -37,7 +36,6 @@ const productosArray = [
     producto9,
     producto10,
 ]
-console.log(JSON.stringify(productosArray))
 
 //! divInicial DOM *primera pagina* - eventos
 const inputNombre = document.getElementById('nombre');
@@ -64,22 +62,23 @@ botonIngresar.onclick = (e) => {
 
 
         //! buscar elementos al DOM
-        const divProductos = document.getElementById('divProductos')
+        // const divProductos = document.getElementById('divProductos')
 
-        productosArray.forEach((producto) => {
+        // productosArray.forEach((producto) => {
 
-            divProductos.innerHTML = divProductos.innerHTML + `
-            <div class="card" style="width: 18rem;">
-            <img src="${producto.img}" class="card-img-top" alt="...">
-            <div class="card-body">
-            <h5>${producto.nombre}</h5>
-              <p class="card-text">${producto.nombre}</p>
-              <p>${producto.precio} USD</p>
-              <button id=${producto.id} class="btn btn-primary">AGREGAR</button>
-            </div>
-          </div>
-        `
-        })
+        //     divProductos.innerHTML = divProductos.innerHTML + `
+        //     <div class="card" style="width: 18rem;">
+        //     <img src="${producto.img}" class="card-img-top" alt="...">
+        //     <div class="card-body">
+        //     <h5>${producto.nombre}</h5>
+        //       <p class="card-text">${producto.nombre}</p>
+        //       <p>${producto.precio} USD</p>
+        //       <button id=${producto.id} class="btn btn-primary">AGREGAR</button>
+        //     </div>
+        //   </div>
+        // `
+        // })
+
 
         //! carrito de compra
 
@@ -95,12 +94,11 @@ botonIngresar.onclick = (e) => {
 
 
                 const productoCarrito = {
-                    id: producto.id,
-                    nombre: producto.nombre,
-                    precio: producto.precio,
+                    id: id,
+                    nombre: nombre,
+                    precio: precio,
                     cantidad: 1
                 }
-
 
                 const indexCarrito = carrito.findIndex(prod => prod.id === producto.id)
                 if (indexCarrito === -1) {
@@ -132,3 +130,60 @@ botonIngresar.onclick = (e) => {
         calcular.append(btnCalcular)
     }
 }
+
+const mostrarCategorias = async () => {
+    const categoriasFetch = await fetch('categorias.json')
+    const categoriasJson = await categoriasFetch.json()
+    categoriasJson.forEach(cat => {
+        const option = document.createElement('option')
+        option.innerText = `${cat}`
+        lista.append(option)
+    })
+}
+
+const buscarTodosProductos = async () => {
+    const productosFetch = await fetch('productos.json')
+    const productosJson = await productosFetch.json()
+    productosJson.forEach(prod => {
+        const { categoria, id, nombre, precio, img } = prod
+        divProductos.innerHTML += `
+        <div class="card" style="width: 18rem;">
+    <img src="${img}" class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5>${nombre}</h5>
+      <p class="card-text">${nombre}</p>
+      <p>${precio} USD</p>
+      <button id=${id} class="btn btn-primary">AGREGAR</button>
+    </div>
+</div>`
+    })
+}
+
+const buscarProductosPorCategoria = async () => {
+    divProductos.innerHTML = ''
+    const categoriaElegida = lista.value
+    const productosFetch = await fetch(`productos.json`)
+    const productosJson = await productosFetch.json()
+    const productosFiltrados = productosJson.filter(prod=>prod.categoria===categoriaElegida)
+    
+    productosFiltrados.forEach(prod => {
+        const { categoria, id, nombre, precio, img } = prod
+        divProductos.innerHTML += `
+        <div class="card" style="width: 18rem;">
+    <img src="${img}" class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5>${nombre}</h5>
+      <p class="card-text">${nombre}</p>
+      <p>${precio} USD</p>
+      <button id=${id} class="btn btn-primary">AGREGAR</button>
+    </div>
+  </div>
+  `
+    })
+}
+
+const lista = document.querySelector('#lista')
+const botonFiltrar = document.querySelector('#filtrar')
+buscarTodosProductos()
+mostrarCategorias()
+botonFiltrar.onclick = buscarProductosPorCategoria
